80896cf620644dd9cccb5a444e46825c6d780f7e
realmatotes <mathias.olausson@lbs.se>
2022-09-01 08:57:40 +0200

    created task

---
+++ FormUppgift/FormUppgift/Program.cs
@@ -0,0 +1,15 @@
+﻿const int STEP_AMOUNTS = 1000;
+
+Console.WriteLine("Formuppgift\n\n");
+Console.Clear();
+
+int currentstep = 0;
+while(currentstep < STEP_AMOUNTS)
+{
+
+
+    currentstep++;
+}
+
+Console.WriteLine("\n\nProgrammet är slut. Tryck på enter");
+Console.ReadLine();
